35e35f167b481378007aa609f8fde7cfa440bcee
Jho00 <akutin_artem@mail.ru>
2023-10-14 17:35:55 +0400

    emulator 2

--- emulator/src/tasks/makeUserFree.js
+++ emulator/src/tasks/makeUserFree.js
@@ -1,18 +1,41 @@
 import {getChildLogger} from "../util/logger.js";
 import {User} from "../db/user.schema.js";
+import {API} from "../api/index.js";
 
 const logger = getChildLogger({ file: "makeUserFree"})
 
 export const makeUserFree = {
     jobName: "Make User Free",
     action: async job => {
-        logger.info("Starting job")
-        const { userId } = job.attrs.data;
-        const user = await User.find({ userId: userId })
+        const { userId, ticketId, ticketTime } = job.attrs.data;
+        logger.info({
+            userId,
+            ticketId,
+            ticketTime
+        },"Starting job")
+
+        if (ticketId) {
+            try {
+                await API.ticketApi.closeTicket(
+                    ticketId,
+                    ticketTime
+                )
+            } catch (e) {
+                logger.error(e)
+            }
+        }
+
+        const user = await User.findOne({ userId: userId })
         user.isFree = true;
         await user.save()
 
-        logger.info(logger.buildMessage("User is free", { userId }))
+        logger.info(logger.buildMessage("User is free",
+            {
+                ticketTime,
+                ticketId,
+                userId
+            }
+        ))
     },
     properties: { priority: 'high', concurrency: 10 }
 }
